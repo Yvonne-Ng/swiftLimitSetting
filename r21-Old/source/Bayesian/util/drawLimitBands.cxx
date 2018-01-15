acadbae4c0693ae2ff53e839c7f6263d8b848aa3
Yvonne Ng <yvonne.ng@cern.ch>
2018-01-15 02:34:33 -0800

    working drawLimitBands.cxx

--- r21-Old/source/Bayesian/util/drawLimitBands.cxx
+++ r21-Old/source/Bayesian/util/drawLimitBands.cxx
@@ -11,6 +11,8 @@
 #include <vector>
 #include <algorithm>
 #include <boost/algorithm/string/replace.hpp>
+#include "TGraphErrors.h"
+#include <fstream>
 /*
 string& replaceString(string& original, string substring1, string substring2)
 { // 1. 
@@ -21,9 +23,18 @@ string& replaceString(string& original, string substring1, string substring2)
 
 using namespace std;
 
+bool does_file_exist(const char *fileName)
+{
+    ifstream infile(fileName);
+    return infile.good();
+
+}
 vector<double> GetCenterAndSigmaDeviations(vector<double> input) 
 {
   std::sort(input.begin(),input.end());
+//for ( int i =0; i< input.size(); ++i){
+//    cout<<"pseudo tests:" <<input.at(i)/35.1<<endl;
+//  }
   unsigned int nVals = input.size();
   vector<double> statVals;
   double wantEvents;
@@ -33,6 +44,7 @@ vector<double> GetCenterAndSigmaDeviations(vector<double> input)
     wantEvents = nVals*quantiles[i];
     bestEvent = (int) wantEvents;
     statVals.push_back(input.at(bestEvent));
+    cout<<"quantile: "<<quantiles[i]<<" pseudoTestValue: "<<input.at(bestEvent)/35.1<<endl;
   }
   return statVals;
 }
@@ -53,22 +65,34 @@ string constructMassFileName(string fileName, string mass){
 }
 
 int main(){
+    cout<<"Configuration"<<endl;
     //---------------------Configurations---------------------------//
-    string inputDir = "/lustre/SCRATCH/atlas/ywng/r21/r21-Old/oldStep2Results";
-//string fileNameTemplate ="Step2_setLimitsOneMassPoint_test2PhPPP_ZprimeCCCMMMM_15p45fb_0.root";
-    string fileNameTemplate =inputDir+"/"+"Step2_setLimitsOneMassPoint_JDMPhPPP_ZprimeCCCMMM_15p45fb_0.root";
-    //TString fileNameTemplate ="Step2_setLimitsOneMassPoint_test2Ph100_Zprime0p31500_15p45fb_0.root";
+    //------r21-Old config---///
+    string inputDir = "/lustre/SCRATCH/atlas/ywng/WorkSpace/r21/r21-Old/source/Bayesian/results/Step2_setLimitsOneMassPoint/test";
+    string fileNameTemplate =inputDir+"/"+"Step2_setLimitsOneMassPoint_JDMPhPPP_ZprimeCCCMMM_35p45fb_0.root";
+    string outputFileNamePrefix="testLimitPlot";
+    string outputFileName;
+    //----end of r21-Old config---//
+    //-------r21SwiftBunny----///
+    //string inputDir = "/lustre/SCRATCH/atlas/ywng/r21/r21SwiftNew/r21StatisticalAnalysis/source/results/Step2_setLimitsOneMassPoint/svnCodebkgndFit";
+    //string fileNameTemplate = inputDir+"/"+"Step2_setLimitsOneMassPoint_JDMPhPPP_ZprimeCCCMMM_35p45fb_0_seedMMM.root";
+    ////string fileNameTemplate =inputDir+"/"+"Step2_setLimitsOneMassPoint_JDMPhPPP_ZprimeCCCMMM_35p45fb_0.root";
+    //string outputFileName="testLimitr21SwiftPlot.root";
 
+    double luminosity=35100;
     //gSMp02_ph100
-    string ptCut = "100";
-    string coupling="0p2";
+    vector<string> ptCutList={"50","100"};
+    vector<string> couplingList={"0p1","0p2", "0p3", "0p4"};
+    
+    //string ptCut = "100";
+    //string coupling="0p3";
    // int masses[]={250, 350, 450, 550,750};
+    
 
-    vector<int> masses{250, 350, 450, 550,750};
-    string fileNameTemplateForRun=initializeFileNameTemplate(fileNameTemplate, ptCut,coupling);
+    vector<int> masses{300, 350,400, 450,500, 550,750, 950,1500};
+//for each pt cut for each coupling for each mass, if file exist then do blah
 
-
-
+    cout<<"end of configuration"<<endl;
     //---------------------End of Configurations-------------------//
     //
     //------------------------Psuedo-Code------------------------//
@@ -88,17 +112,98 @@ int main(){
 
 //maybe put this into a function 
 //
-for (int iMassPoint=0; iMassPoint<masses.size(); iMassPoint++){
+//for (vector<char>::iterator ptCut = ptCutList.begin(); ptCut != ptCutList.end(); ++ptCut){
+//for (vector<char>::iterator coupling = couplingList.begin(); coupling = couplingList.end(); ++coupling){
+for (auto ptCut: ptCutList){
+for (auto coupling: couplingList){
+
+    string fileNameTemplateForRun=initializeFileNameTemplate(fileNameTemplate, ptCut,coupling);
+    outputFileName= outputFileNamePrefix+ string("_") +string(ptCut)+string("_")+string(coupling)+string(".root");
+    cout<<"coupling: "<<coupling<<" ptCut: "<<ptCut<<endl;
+    int iMassExist =0; //iMassCounter that exists
+
+    cout<<"creating makeing histogram"<<endl; //declaring the histograms here 
+    TGraphErrors * observed = new TGraphErrors();
+    TGraphAsymmErrors * expectedWithOneSigma = new TGraphAsymmErrors();
+    TGraphAsymmErrors * expectedWithTwoSigma = new TGraphAsymmErrors();
+for (int iMassPoint=0; iMassPoint<masses.size(); iMassPoint++){//This mass point loop through the mass points that are in the list (does not always exists)
     int iMass = masses.at(iMassPoint);
     cout<<"MAss: "<<iMass<<endl;
     string iMassFileName=constructMassFileName(fileNameTemplateForRun,to_string(iMass));
+    if (does_file_exist(iMassFileName.c_str())) {
+        vector<double> CLPseudo;
+        double CLObserved = -1;
 
+
+    //cout<<"opening file "<<iMassFileName<<endl;
 // construct TFile f
     TFile f(iMassFileName.c_str(), "READ");
 //Get the observed Limit
-    //(TVectorD *)
+///
+    //cout<<"getting CLOfRealLikelihood"<<endl;
+    TVectorD * CLFromFile = (TVectorD*) f.Get("CLOfRealLikelihood");
+    CLObserved = (*CLFromFile)[0]/luminosity;
+//#cout<<"CLObserved: "<<CLObserved<<endl;
+    
+    //Set points for the theory line
+    observed->SetPoint(iMassExist, iMass, CLObserved);
 
+    cout<<"pt #: "<<iMassExist <<" mass: "<<iMass<<" CLObserved: "<<CLObserved<<endl;
+    observed->SetPointError(iMassExist, 0, 0);
 
-}
-    return 0;
+    // Read tree from file
+    //cout<<"getting ensemble test"<<endl;
+    TTree * tree = (TTree*) f.Get("ensemble_test");
+    double CL;
+
+    //cout<<"getting 95quantile_marginalized_2"<<endl;
+    tree->SetBranchAddress("95quantile_marginalized_1", &CL);
+    for (int i = 0; i<tree->GetEntries(); i++) {
+      tree->GetEvent(i);
+      CLPseudo.push_back(CL);
+      //std::cout << "individual pseudo CL = " << CL << "/" << luminosity << " = " << CL/luminosity << std::endl;
+    }
+    f.Close();
+
+      vector<double> bandLimits = GetCenterAndSigmaDeviations(CLPseudo);
+
+      for(std::vector<double>::size_type p=0; p!=bandLimits.size(); ++p) bandLimits[p] /= luminosity;
+
+      for(std::vector<double>::size_type p=0; p!=bandLimits.size(); ++p)  cout<<"bandLimits: "<<bandLimits[p]<<endl;
+
+      // Set points in expected graphs
+      expectedWithOneSigma->SetPoint(iMassExist, iMass, bandLimits.at(2));
+      expectedWithOneSigma->SetPointError(iMassExist, 0, 0, (bandLimits.at(2)-bandLimits.at(1)),
+                               (bandLimits.at(3)-bandLimits.at(2)));
+      expectedWithTwoSigma->SetPoint(iMassExist, iMass, bandLimits.at(2));
+      expectedWithTwoSigma->SetPointError(iMassExist, 0, 0, (bandLimits.at(2)-bandLimits.at(0)),
+                               (bandLimits.at(4)-bandLimits.at(2)));
+
+      std::cout << "Center of expected, observed: " << bandLimits.at(2) << " " << CLObserved << std::endl;
+      iMassExist++;  
+    }// if does file exist
+} //loop for imass
+  std::cout << "Writing output file " << outputFileName  << std::endl;
+
+  TFile outfile(outputFileName.c_str(),"RECREATE");
+  outfile.cd();
+
+  observed->SetName("observedXSecAccVersusMass");
+  observed->Print();
+  observed->Write();
+  expectedWithOneSigma->SetName("expectedXSecAccVersusMass_oneSigma");
+  expectedWithOneSigma->Print();
+  expectedWithOneSigma->Write();
+  expectedWithTwoSigma->SetName("expectedXSecAccVersusMass_twoSigma");
+  expectedWithTwoSigma->Print();
+  expectedWithTwoSigma->Write();
+
+  outfile.Close();
+  delete observed;
+  delete expectedWithOneSigma;
+  delete expectedWithTwoSigma;
+} //loop for ptCu
+}// loop for coupling 
+
+  return 0;
 }//end of main
